6a286a86f02c6585a2821f022bbb4e474ada7a58
seciadev <seciadev@gmail.com>
2018-01-17 22:13:24 +0100

    Post editing enabled

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 	url(r'about/', views.about, name='about'),
 	url(r'news/$', views.news, name='news'),
 	url(r'^news/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^news/new/$', views.post_new, name='post_new'),
+	url(r'^news/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
